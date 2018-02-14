23d192911c99978891775d5eaa9c72fbe8e7d165
Ronan <17938131+ronan18@users.noreply.github.com>
2018-02-14 13:10:48 -0800

    Update typing.vue

--- src/components/typing.vue
+++ src/components/typing.vue
@@ -13,7 +13,7 @@
         <div class="textToType">
           <p>{{testText}}</p>
         </div>
-        <input id="typingInput" autofocus type="text" name="text" value="" placeholder="type here">
+        <input onCopy="return false" onDrag="return false" onDrop="return false" onPaste="return false" autocomplete="off" v-model="userText" id="typingInput" @keyup.enter="submit" @keyup="startTimer" autofocus type="text" name="text" value="" placeholder="type here">
       </div>
 
     </div>
@@ -27,20 +27,58 @@
 </div>
 </template>
 <script>
+//TODO: https://github.com/kpdecker/jsdiff
 import '../assets/typing.css'
 export default {
   name: "typing",
   data: () => ({
     testing: false,
-    testText: 'autofocus',
+    testText: '',
+    testWordCount: 0,
+    userText: '',
+    userWordCount: 0,
+    error: null,
+    startedTime: 0,
+    endTime: 0,
+    result: null,
+    started: false,
+    wpm: null,
     scentences: [
       'Once along long time ago there was a child. This child really liked cheese but he was not able to eat his cheese because Google Docs was taking forver to load. Then he used Graphite Writer which loaded much faster, allowing him to eat as much cheese as he wanted.'
     ]
   }),
   methods: {
+    submit: function () {
+      let clickedTime = Date.now();
+      this.result = (clickedTime - this.startedTime) / 1000;
+      function WordCount(str) {
+  return str.split(" ").length;
+}
+
+this.testWordCount = WordCount(this.testText)
+this.userWordCount = WordCount(this.userText)
+      //console.log(this.userText);
+      this.error = this.userText !== this.testText
+      //console.log(this.error);
+this.wpm = (this.userWordCount/this.result) * 60
+      console.log(this.result, this.error, this.testWordCount,this.userWordCount, this.wpm + ' WPM');
+
+
+    },
+    startTimer: function() {
+      if (!this.started) {
+         this.startedTime = Date.now();
+         this.started = true
+         console.log('started');
+      }
+
+    },
     startTest: function() {
       this.testText = this.scentences[Math.floor(Math.random() * this.scentences.length)]
       this.testing = true;
+
+
+
 
       //document.getElementById("typingInput").focus();
     }
